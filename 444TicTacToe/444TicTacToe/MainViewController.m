1130544803fb4aa4b2524c7fe72ed6fa3287186b
Austin Wright <austin.wright@mobileheartbeat.com>
2017-02-24 12:54:40 -0500

    Working v1 todo: add 2x2 win conditions fix BButton fix Font Awesome Icons

--- 444TicTacToe/444TicTacToe/MainViewController.m
+++ 444TicTacToe/444TicTacToe/MainViewController.m
@@ -134,8 +134,33 @@
 
     if(currentCellState == GAME_CELL_STATE_EMPTY) {
         self.gameStateMatrix[(NSUInteger) cellRow][(NSUInteger) cellColumn] = @(newCellState);
-        self.isPlayerXTurn = !self.isPlayerXTurn;
         cell.userInteractionEnabled = NO;
+
+        if([self checkForWinCondition]) {
+            NSString *currentPlayerString = self.isPlayerXTurn ? @"X" : @"O";
+            NSString *winString = [NSString stringWithFormat:@"Player %@ Wins!", currentPlayerString];
+            UIAlertController *winAlert = [UIAlertController alertControllerWithTitle:winString
+                                                                              message:@"Play Again?"
+                                                                       preferredStyle:UIAlertControllerStyleAlert];
+
+            UIAlertAction * startNewGameAction = [UIAlertAction actionWithTitle:@"New Game"
+                                                                          style:UIAlertActionStyleDefault
+                                                                        handler:^(UIAlertAction *action) {
+                                                                            [self startNewGameButtonClicked:self];
+                                                                        }];
+
+            UIAlertAction * cancelAction = [UIAlertAction actionWithTitle:@"Not Yet ..."
+                                                                          style:UIAlertActionStyleCancel
+                                                                        handler:nil];
+
+            [winAlert addAction:cancelAction];
+            [winAlert addAction:startNewGameAction];
+            [self presentViewController:winAlert animated:YES completion:nil];
+
+        } else {
+            self.isPlayerXTurn = !self.isPlayerXTurn;
+        }
+
         [collectionView reloadData];
     }
 }
@@ -156,10 +181,14 @@
     if (kind == UICollectionElementKindSectionFooter) {
         StartNewGameFooterView *footerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter
                                                                                   withReuseIdentifier:@"StartNewGameFooterView" forIndexPath:indexPath];
-        [footerView.startNewGameButton setStyle:BButtonStyleBootstrapV3];
-        [footerView.startNewGameButton setButtonCornerRadius:@0];
-        [footerView.startNewGameButton setBackgroundColor:[UIColor colorWithRed:0.0f green:189/255 blue:172/255 alpha:1.0f]];
-        [footerView.startNewGameButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//        footerView.startNewGameButton = [BButton buttonWithType:UIButtonTypeCustom];
+//        [footerView.startNewGameButton setStyle:BButtonStyleBootstrapV3];
+//        [footerView.startNewGameButton setButtonCornerRadius:@0];
+//        // todo: find out why BButton is misbehaving
+//        [footerView.startNewGameButton setBackgroundColor:[UIColor cyanColor]];//[UIColor colorWithRed:0.0f green:189/255 blue:172/255 alpha:0.0f]];
+//        [footerView.startNewGameButton titleLabel].backgroundColor = [UIColor cyanColor];
+//        [footerView.startNewGameButton titleLabel].textColor = [UIColor whiteColor];
+//        [footerView.startNewGameButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal | UIControlStateSelected | UIControlStateHighlighted];
 
         reusableView = footerView;
     }
@@ -180,22 +209,77 @@
 }
 
 
-- (IBAction)startNewGameButtonClicked:(BButton*)sender {
+- (IBAction)startNewGameButtonClicked:(id)sender {
     [self initializeGameState];
     [self.collectionView reloadData];
 }
 
 -(BOOL)checkForWinCondition {
-    return NO;
-    // check for horizontal
 
-    //check for vertical
 
+    // self.gameStateMatrix[<Y INDEX>][<X INDEX>]
+    // if any count adds up to |4|, return true
+
+    // finite checks first
     //check for 0,0 diagonal
+    int backSlashDiagonal = self.gameStateMatrix[0][0].intValue + self.gameStateMatrix[1][1].intValue +
+            self.gameStateMatrix[2][2].intValue + self.gameStateMatrix[3][3].intValue;
+    if(abs(backSlashDiagonal) >= 4) {
+        return YES;
+    }
+
 
     //check for 3,0 diagonal
+    int forwardSlashDiagonal = self.gameStateMatrix[0][3].intValue + self.gameStateMatrix[1][2].intValue +
+            self.gameStateMatrix[2][1].intValue + self.gameStateMatrix[3][0].intValue;
+    if(abs(forwardSlashDiagonal) >= 4) {
+        return YES;
+    }
 
     //check for corners
+    int fourCorners = self.gameStateMatrix[0][0].intValue + self.gameStateMatrix[0][3].intValue +
+            self.gameStateMatrix[3][0].intValue + self.gameStateMatrix[3][3].intValue;
+    if(abs(fourCorners) >= 4) {
+        return YES;
+    }
+
+    // todo: check for 2x2
+    // 0,0 ; 0,1 ; 1,0 ; 1,1 to 0,2 ; 0,3 ; 1,2 ; 1,3
+    // 1,0 ; 1,1 ; 2,0 ; 2,1 to 1,2 ; 1,3 ; 2,2 ; 2,3
+    // 2,0;2,1;2,0;2,1 to 0,2;0,3;1,2;1,3
+    // 2,0;2,1;2,0;2,1 to 0,2;0,3;1,2;1,3
+    int squareCount = self.gameStateMatrix[0][0].intValue + self.gameStateMatrix[0][1].intValue +
+            self.gameStateMatrix[1][0].intValue + self.gameStateMatrix[1][1].intValue;
+
+    // 1st pass brute force:
+    // check for horizontal
+    //check for vertical
+    NSMutableArray <NSNumber*>* verticalCountArray = [NSMutableArray arrayWithCapacity:self.gameStateMatrix.count];
+    // init the array just in case
+    // todo verify if needed
+    for(id i in self.gameStateMatrix) {
+        [verticalCountArray addObject:@0];
+    }
+    for(int rowIndex = 0; rowIndex < self.gameStateMatrix.count; rowIndex++) {
+        int horizontalCount = 0;
+        for(int columnIndex = 0; columnIndex < self.gameStateMatrix[0].count ;columnIndex ++) {
+            NSNumber * cellValue = self.gameStateMatrix[(NSUInteger) rowIndex][(NSUInteger) columnIndex];
+            NSNumber * verticalCount = verticalCountArray[(NSUInteger) columnIndex];
+            horizontalCount += [cellValue intValue];
+            verticalCountArray[(NSUInteger) columnIndex] = @([cellValue intValue] + [verticalCount intValue]); // increment the column counter
+        }
+        // done with a row, check for win
+        if(abs(horizontalCount) >= 4) {
+            return YES;
+        }
+    }
+    for(NSNumber* verticalCount in verticalCountArray) {
+        if(abs([verticalCount intValue]) >= 4) {
+            return YES;
+        }
+    }
+
+    return NO;
 }
 
 @end
